6501d60ed776ed60f858ca85fdb2bb306c3607b7
Otari Kobiashvili <otarikobi@gmail.com>
2017-10-11 01:16:34 -0400

    Animate destination images

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,13 +7,15 @@ function animateOnScroll(){
   var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0,
       sectionTitles = document.querySelectorAll("section h3.title"),
       sectionHRs = document.querySelectorAll("section hr"),
-      sectionParagraphs = document.querySelectorAll("section p");
+      sectionParagraphs = document.querySelectorAll("section p"),
+      destImages = document.querySelectorAll(".destinations .grid li");
 
 
 
   animateElements(sectionTitles, "slide-in");
   animateElements(sectionHRs, "slide-in");
   animateElements(sectionParagraphs, "fade-in");
+  animateElements(destImages, "slide-in")
 };
 
 function animateElements(els, animation){
