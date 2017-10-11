ab9a5cdec658755a25938942fb6b978824bde860
Otari Kobiashvili <otarikobi@gmail.com>
2017-10-11 01:06:13 -0400

    Add slidein and fade in animation to all section h3, p, and hr

---
+++ assets/scripts/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+
+window.addEventListener("scroll", animateOnScroll, false);
+
+function animateOnScroll(){
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0,
+      sectionTitles = document.querySelectorAll("section h3.title"),
+      sectionHRs = document.querySelectorAll("section hr"),
+      sectionParagraphs = document.querySelectorAll("section p");
+
+
+
+  animateElements(sectionTitles, "slide-in");
+  animateElements(sectionHRs, "slide-in");
+  animateElements(sectionParagraphs, "fade-in");
+};
+
+function animateElements(els, animation){
+  for(var i = 0; i < els.length; i++) {
+    if(isScrolledIntoView(els[i])){
+      els[i].classList.add(animation);
+    }
+  }
+}
+
+
+function isScrolledIntoView(el) {
+  var elemTop = el.getBoundingClientRect().top;
+  var elemBottom = el.getBoundingClientRect().bottom;
+
+  var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+
+  return isVisible;
+}
